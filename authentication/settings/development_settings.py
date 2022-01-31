cbe83b02969840179236f0a04586fe32cd49640d
mohsen baghdadi <mohsen.baghdadi@gmail.com>
2022-01-31 15:04:05 +0330

    Update development_settings.py

--- authentication/settings/development_settings.py
+++ authentication/settings/development_settings.py
@@ -7,9 +7,9 @@ CORS_ORIGIN_ALLOW_ALL = DEBUG
 
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-DEFAULT_FROM_EMAIL = "noreply@khallagh.com"
-EMAIL_HOST = "mail.khallagh.com"
-EMAIL_HOST_USER = "noreply@khallagh.com"
-EMAIL_HOST_PASSWORD = "Khallagh@dashboard@123"
+DEFAULT_FROM_EMAIL = ""
+EMAIL_HOST = ""
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 EMAIL_PORT = 25
